051845f62dcdc0091a9e8ad0c64e2273cdb7daff
Gene <gene.li@nyu.edu>
2025-09-15 21:09:43 +0900

    dummy data test of language provider/toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LanguageToggle from './components/LanguageToggle'
+import { LanguageProvider } from './context/LanguageContext'
+import PersonalInfo from './components/PersonalInfo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <LanguageToggle/>
-
+      <LanguageProvider>
+      <LanguageToggle />
+      <PersonalInfo
+        name="Gene Fang"
+        dob="1990/01/01"
+        region="Kobe"
+        phone="090-xxxx-xxxx"
+        email="gene.fang@example.com"
+      />
+      {/* other components */}
+    </LanguageProvider>
     </>
   )
 }
